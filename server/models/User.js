bf0d6b093594a689aa9662d320ed858dcd0b0251
hamad <hamadkhadimdgkmc@gmail.com>
2025-12-22 19:37:57 +0500

    Updated userschemia  backend

--- server/models/User.js
+++ server/models/User.js
@@ -1,19 +1,40 @@
-import mongoose, { Schema } from 'mongoose'
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;  // Add this line
 
-const userSchemia = new mongoose.Schemia(
-  {
-_id       : { type : String  , required : true},
-name       : { type : String  , required : true},
-email       : { type : String  , required : true},
-imageUrl      : { type : String  , required : true},
-enrolledCourses :[
-  {
-    type : mongoose.Schema.Types.ObjectId ,
-    ref : 'course'
-  }
-]
+const userSchema = new Schema({  // Use Schema instead of mongoose.Schema
+  _id: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  enrolledCourses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'course'  // Changed 'course' to 'Course' for consistency
+    }
+  ]
+}, { timestamps: true });
 
-} ,{timestamps : true});
+const User = mongoose.model('User', userSchema);
+export default User;
 
-const User = mongoose.model('User' , userSchemia);
-export default User
+
+
+// import mongoose, { Schema } from 'mongoose'
+
+// const userSchemia = new mongoose.Schemia(
+//   {
+// _id       : { type : String  , required : true},
+// name       : { type : String  , required : true},
+// email       : { type : String  , required : true},
+// imageUrl      : { type : String  , required : true},
+// enrolledCourses :[
+//   {
+//     type : mongoose.Schema.Types.ObjectId ,
+//     ref : 'course'
+//   }
+// ]
+
+// } ,{timestamps : true});
+
+// const User = mongoose.model('User' , userSchemia);
+// export default User
